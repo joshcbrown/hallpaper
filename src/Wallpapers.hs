709ead4ca1ae04fb2c6a05fbfd6277b6001507f2
Josh Brown <joshcarlbrown@gmail.com>
2024-01-04 12:45:35 +1100

    try implement daemon-client service
    dbus says the method isn't recognised and not sure how to debug

--- src/Wallpapers.hs
+++ src/Wallpapers.hs
@@ -1,13 +1,20 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Wallpapers where
 
+import Control.Concurrent (MVar, ThreadId, forkIO, killThread, modifyMVar_, newEmptyMVar, newMVar)
+import Control.Concurrent.MVar (takeMVar)
 import qualified Control.Foldl as Fold
+import DBus
+import DBus.Client
 import qualified GI.Notify as Notify
 import System.FilePath (takeExtension)
 import System.Random
 import System.Random.Shuffle
-import Turtle
+import Turtle hiding (export)
 
 data PomoState = Work | ShortBreak | LongBreak
+data ThreadState = ThreadState {inPomo :: Bool, threadId :: ThreadId, files :: [FilePath]}
 
 times :: [PomoState]
 times = cycle [Work, ShortBreak, Work, ShortBreak, Work, ShortBreak, Work, LongBreak]
@@ -17,34 +24,86 @@ stateTime Work = 20 * 60
 stateTime ShortBreak = 5 * 60
 stateTime LongBreak = 15 * 60
 
-notify :: PomoState -> IO ()
-notify state =
-    let message = case state of
-            Work -> "work"
-            ShortBreak -> "short break"
-            LongBreak -> "long break"
-     in Notify.notificationShow =<< Notify.notificationNew message Nothing Nothing
+notifyPomo :: PomoState -> IO ()
+notifyPomo Work = notify "work"
+notifyPomo ShortBreak = notify "short break"
+notifyPomo LongBreak = notify "long break"
 
-display :: FilePath -> PomoState -> IO ()
-display file state =
+notify :: Text -> IO ()
+notify message = Notify.notificationShow =<< Notify.notificationNew message Nothing Nothing
+
+display :: FilePath -> IO ()
+display file =
     procs
         "swww"
         ["img", format fp file, "--transition-fps", "140", "--transition-type", "center"]
         empty
-        >> notify state
+
+displayPomo :: FilePath -> PomoState -> IO ()
+displayPomo file state =
+    display file
+        >> notifyPomo state
         >> sleep (stateTime state)
+
+displayNormal :: FilePath -> IO ()
+displayNormal file = display file >> sleep (20 * 60)
 
 isImg :: FilePath -> Bool
 isImg = (`elem` [".gif", ".png", ".jpg", ".jpeg"]) . takeExtension
+
+toggle :: MVar ThreadState -> IO ()
+toggle v = modifyMVar_ v $ \s@(ThreadState{..}) -> do
+    killThread threadId
+    shuffled <- shuffle' files (length files) <$> newStdGen
+    let newJob = if inPomo then mapM_ (uncurry displayPomo) (align shuffled) else mapM_ display shuffled
+    newThread <- forkIO newJob
+    when inPomo $ notify "leaving pomo"
+    pure $ s{inPomo = not inPomo, threadId = newThread}
+  where
+    align = (`zip` times) . cycle
+
+setupDBus :: ThreadState -> IO ()
+setupDBus state = do
+    client <- connectSession
+    let serviceName = busName_ "org.jbrown.hallpaper"
+    -- the options we have here ensure that we don't need to check the result
+    -- of the name request
+    void $ requestName client serviceName [nameAllowReplacement, nameReplaceExisting, nameDoNotQueue]
+    stateVar <- newMVar state
+    export
+        client
+        "/togglePomo"
+        defaultInterface
+            { interfaceName = "org.jbrown.toggle"
+            , interfaceMethods =
+                [autoMethod "toggle" (toggle stateVar)]
+            }
+
+start :: [FilePath] -> IO ThreadState
+start files = do
+    shuffled <- shuffle' files (length files) <$> newStdGen
+    threadId <- forkIO (mapM_ display shuffled)
+    pure ThreadState{threadId = threadId, files = files, inPomo = False}
 
 daemonMain :: IO ()
 daemonMain = do
     homePath <- home
     let fstream = ls (homePath ++ "/.config/sway/wallpapers")
     fs <- filter isImg <$> fold fstream Fold.list
-    shuffled <- shuffle' fs (length fs) <$> newStdGen
+    state <- start fs
+    setupDBus state
+    -- somewhat hacky way to make the main thread block indefinitely
+    newEmptyMVar >>= takeMVar
 
-    Notify.init $ Just "wallpaperd"
-    mapM_ (uncurry display) (align shuffled)
-  where
-    align = (`zip` times) . cycle
+clientMain :: IO ()
+clientMain = do
+    client <- connectSession
+
+    reply <- call client (methodCall "/togglePomo" "org.jbrown.toggle" "toggle")
+
+    -- Handle the reply
+    case reply of
+        Left error -> putStrLn $ "Error: " ++ show error
+        Right result -> putStrLn $ "Received reply: " ++ show result
+
+    disconnect client
